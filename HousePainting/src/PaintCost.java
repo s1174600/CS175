e9192052040b532930dcd2af4cb30e0dcec79f6e
Ethan Riedel <s1174600@monmouth.edu>
2019-09-16 20:56:54 -0400

    Variables created

---
+++ HousePainting/src/PaintCost.java
@@ -0,0 +1,15 @@
+
+public class PaintCost {
+
+	public static void main(String[] args) {
+		/*
+		 * This program will calculate the cost to paint a house
+		 */
+        int houseLengthFeet=20;
+        double inches = 6;
+        System.out.println(houseLengthFeet + inches/12);
+        
+        
+	}
+
+}
