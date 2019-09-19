205129f00ae9564732d07dbb939513b44695d671
Ethan Riedel <s1174600@monmouth.edu>
2019-09-18 21:02:31 -0400

    Wat Up

--- HousePainting/src/PaintCost.java
+++ HousePainting/src/PaintCost.java
@@ -9,6 +9,47 @@ public class PaintCost {
         double inches = 6;
         System.out.println(houseLengthFeet + inches/12);
         
+        int houseWidthFeet=10;
+        double inches1 = 6;
+        System.out.println(houseWidthFeet + inches1/12);
+        
+        int houseHeightFeet=15;
+        double inches2 = 6;
+        System.out.println(houseHeightFeet + inches2/12);
+        
+        int windowLengthFeet=4;
+        double inches3 = 6;
+        System.out.println(windowLengthFeet + inches3/12);
+        
+        int windowWidthFeet=2;
+        double inches4 = 6;
+        System.out.println(windowWidthFeet + inches4/12);
+        
+        int doorLengthFeet=8;
+        double inches5 = 6;
+        System.out.println(doorLengthFeet + inches5/12);
+        
+        int doorWidthFeet=4;
+        double inches6 = 6;
+        System.out.println(doorWidthFeet + inches6/12);
+        
+        
+        
+        
+		
+		
+        
+        
+        
+        
+      
+        
+        
+        
+        
+        
+        
+        
         
 	}
 
