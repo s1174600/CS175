e202e76415beaeb2a1cb6f29e508d7f41137d19e
Ethan Riedel <s1174600@monmouth.edu>
2019-09-30 20:34:23 -0400

    Question 4.4 HW

---
+++ CS175/src/ch4/a43.java
@@ -0,0 +1,23 @@
+package ch4;
+
+import java.util.Scanner;
+
+public class a43 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner keyIn = new Scanner(System.in);
+		
+		System.out.println("Please enter a number now: ");
+		
+		int number = keyIn.nextInt();
+		
+		System.out.println("number^2: " + Math.pow(number, 2));
+		
+		System.out.println("number^2: " + number * number);
+		
+		System.out.println("number^4: " + Math.pow(number, 4));
+	}
+
+}
