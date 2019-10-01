2bc94a9723d761f42e2b70bbaab4c03e6b575034
Ethan Riedel <s1174600@monmouth.edu>
2019-09-30 20:32:08 -0400

    If and Else practice

---
+++ HousePainting/src/Itest.java
@@ -0,0 +1,13 @@
+
+public class Itest {
+
+	public static void main(String[] args) {
+		int a=20;
+		int b=10;
+		if (a>b) {System.out.println("A is greater than B");}
+		else if (a<b) {System.out.println("A is less than B");}
+		else {System.out.println("A is eual to B");}
+
+	}
+
+}
