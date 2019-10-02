a8adbef8d0d77052bd44c0b139f2db3e1ac3a7ec
riede <riede@10.0.0.170>
2019-10-02 01:42:39 -0400

    Seat Belt System

--- CS175/src/FSM.java
+++ CS175/src/FSM.java
@@ -1,93 +1,122 @@
-
 import java.util.Scanner;
 
 public class FSM {
-	
-	public enum State {
-		STATE0, off, STATE1, seated, STATE2, ready, STATE3, belted, STATE4, timing, STATE5, buzzer, MAX_STATE;
-	}
-	public enum Input {
-		Input0, terminate, INPUT1, key, INPUT2, seat, INPUT3, unseat, INPUT4, belt, INPUT5, unbelt, INPUT6, timer_expires, MAX_INPUT;
-
-		public static Input fromInt(int num) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-	}
+	public static void main(String[] args) {
 	State state;
-	Input input;
+	Input input = null;
+
 	
-	FSM(){
-		state = State.STATE0;
+	FSM();{
+	state = State.off;
 	}
 	
-	public void run(){
+	
+	
+	
 		Scanner in = new Scanner(System.in);
-		int num;
-
+		int num;{
+		
 		while (state != State.MAX_STATE){
-			if (state != State.STATE4){
+			if (state != State.STATE6){
 				//get input
 				while (true){
-					System.out.println("You are currently in the state of "+state);
-					System.out.println("Please enter a number in 0-9: ");
+					System.out.println("You are currently in the state of " + state);
+					System.out.println("Please enter a number from the following options: 1 -- key 2 -- seat 3 -- unseat 4 -- belt 5 -- unbelt 6 -- timer_expires 0 -- terminate");
 					num = in.nextInt();
-					if (num >= 0 && num < 10) break;
+					if (num >= 0 && num < 7) break;
 					else {
 						System.out.print("Wrong input! ");
 					}
 				}
 				
-				input = Input.fromInt(num);
+				input = Input.fromInteger(num);
 			}
 			
 			switch(state){
-			case STATE0:
-				if (input == Input.INPUT1){
-					state = State.STATE1;
+			case STATE0: //0ff
+				if (input == Input.Input0){  //terminate
+					state = State.STATE1;  //seated
 				}
 				else{
 					System.out.println("Wrong! Start over! "+state);
-					state = State.STATE0;
+					state = State.off;
 				}
 				break;
 			case STATE1:
-				if (input == Input.Input0){
-					state = State.STATE2;
+				if (input == Input.INPUT1){  //key
+					state = State.STATE2;  //ready
 				}
 				else{
 					System.out.println("Wrong! Start over! "+state);
-					state = State.STATE0;
+					state = State.off;
 				}	
 				break;		
 			case STATE2:
-				if (input == Input.INPUT1){
-					state = State.STATE3;
+				if (input == Input.INPUT2){  //seat
+					state = State.STATE3;  //belted
 				}
 				else{
 					System.out.println("Wrong! Start over! "+state);
-					state = State.STATE0;
+					state = State.off;
 				}
 				break;
 			case STATE3:
-				if (input == Input.INPUT5){
-					state = State.STATE4;
+				if (input == Input.INPUT3){  //unseat
+					state = State.STATE4;  //timing
 				}
 				else{
 					System.out.println("Wrong! Start over! "+state);
-					state = State.STATE0;
+					state = State.off;
 				}
 				break;
 			case STATE4:
+				if (input == Input.INPUT4){  //belt
+					state = State.STATE5;  //buzzer
+				}
+				else{
+					System.out.println("Wrong! Start over! "+state);
+					state = State.off;
+				}
+				break;
+			case STATE5: 
+				if (input == Input.INPUT5){  //unbelt
+					state = State.STATE6;  //max state
+				}
+				else{
+					System.out.println("Wrong! Start over! "+state);
+					state = State.off;
+				}
+				break;
+			
+			case STATE6:
 				state = State.MAX_STATE;
-				System.out.println("The safe is open!");
+				System.out.println("The seat belt system has been implemented");
 				break;
 			default:
 				System.out.println("Wrong input!");
 				break;
+				
 			}
 		}
 		in.close();	
+		}
 	}
+		
+	
+	
+		
+	
+
+	private static void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void FSM() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 }
 
